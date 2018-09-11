008a28362636759ba7b763601061c849892ddd33
Stefan Matic <maticst@NWW135>
2018-09-11 14:32:09 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,42 +1,50 @@
-var http = require('http');
-var express = require('express');
+var http = require("http");
+var express = require("express");
 var app = express();
 var server = http.createServer(app).listen(3001);
-var io = require('socket.io').listen(server);
-io.set('log level', 1);
-io.set('transports', ['websocket']);
+var io = require("socket.io").listen(server);
+io.set("log level", 1);
+io.set("transports", ["websocket"]);
 
-app.configure(function(){
-    app.use(function(req, res, next) {
-        res.setHeader("X-UA-Compatible", "chrome=1");
-        return next();
-    });
-    app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  res.setHeader("X-UA-Compatible", "chrome=1");
+  return next();
 });
-io.sockets.on('connection', function(socket) {
-    socket.on('CreateSession', function(msg){
-        socket.join(msg);
-    });
-    socket.on('PageChange', function(msg){
-        socket.join(msg);
-        io.sockets.in(msg).emit('SessionStarted', '');
-        console.log('PageChange');
-    });
-    socket.on('JoinRoom', function(msg){
-        socket.join(msg);
-        io.sockets.in(msg).emit('SessionStarted', '');
-    });
-    socket.on('ClientMousePosition', function(msg){
-        socket.broadcast.to(socket.room).emit('ClientMousePosition', {PositionLeft:msg.PositionLeft, PositionTop:msg.PositionTop});
-    });
-    socket.on('AdminMousePosition', function(msg){
-        socket.broadcast.to(msg.room).emit('AdminMousePosition', {PositionLeft:msg.PositionLeft, PositionTop:msg.PositionTop});
-    });
-    socket.on('changeHappened', function(msg){
-        socket.broadcast.to(msg.room).emit('changes', msg.change);
-    });
-    socket.on('DOMLoaded', function(msg){
-        socket.broadcast.to(msg.room).emit('DOMLoaded', '');
-    });
+app.use(express.static(__dirname + "/public"));
+io.sockets.on("connection", function(socket) {
+  socket.on("CreateSession", function(msg) {
+    socket.join(msg);
+  });
+  socket.on("PageChange", function(msg) {
+    socket.join(msg);
+    io.sockets.in(msg).emit("SessionStarted", "");
+    console.log("PageChange");
+  });
+  socket.on("JoinRoom", function(msg) {
+    socket.join(msg);
+    io.sockets.in(msg).emit("SessionStarted", "");
+  });
+  socket.on("ClientMousePosition", function(msg) {
+    socket.broadcast
+      .to(socket.room)
+      .emit("ClientMousePosition", {
+        PositionLeft: msg.PositionLeft,
+        PositionTop: msg.PositionTop
+      });
+  });
+  socket.on("AdminMousePosition", function(msg) {
+    socket.broadcast
+      .to(msg.room)
+      .emit("AdminMousePosition", {
+        PositionLeft: msg.PositionLeft,
+        PositionTop: msg.PositionTop
+      });
+  });
+  socket.on("changeHappened", function(msg) {
+    socket.broadcast.to(msg.room).emit("changes", msg.change);
+  });
+  socket.on("DOMLoaded", function(msg) {
+    socket.broadcast.to(msg.room).emit("DOMLoaded", "");
+  });
 });
 app.listen(3000);
--- public/lib/js/screenshare.js
+++ public/lib/js/screenshare.js
@@ -1,9 +1,8 @@
 var socket = undefined;
 var SessionKey;
 var oDOM;
-var CDN = 'http://yearofthecu.com:3000/';
-var SocketCDN = 'http://yearofthecu.com:3001/'
-
+var CDN = "http://localhost:3000/";
+var SocketCDN = "http://localhost:3001/";
 
 /* This block of code is to load inaccessible scripts that may be relative on mobile devices or behind a VPN or restricted network
  function getScriptURL(){
@@ -18,214 +17,232 @@ var SocketCDN = 'http://yearofthecu.com:3001/'
  */
 
 function loadScript(sScriptSrc, oCallback) {
-    var oHead = document.getElementsByTagName('head')[0];
-    var oScript = document.createElement('script');
-    oScript.type = 'text/javascript';
-    oScript.src = sScriptSrc;
-    oScript.onload = oCallback;
-    oScript.onreadystatechange = function() {
-        if (this.readyState == 'loaded' ||this.readyState == 'complete') {
-            oCallback();
-        }
-    };
-    oHead.appendChild(oScript);
+  var oHead = document.getElementsByTagName("head")[0];
+  var oScript = document.createElement("script");
+  oScript.type = "text/javascript";
+  oScript.src = sScriptSrc;
+  oScript.onload = oCallback;
+  oScript.onreadystatechange = function() {
+    if (this.readyState == "loaded" || this.readyState == "complete") {
+      oCallback();
+    }
+  };
+  oHead.appendChild(oScript);
 }
 
-function LoadAllScripts(){
-    if (typeof jQuery == 'undefined') {
-        loadScript(CDN + 'lib/js/jquery.js', function(){
-            loadScript(CDN + 'lib/js/mutation_summary.js', function(){
-                loadScript(CDN + 'lib/js/tree_mirror.js', function(){
-                    loadScript(SocketCDN + 'socket.io/socket.io.js', function(){
-                        window.addEventListener('load', function() {
-                            startMirroring();
-                        });
-                        init();
-                    });
-                })
+function LoadAllScripts() {
+  if (typeof jQuery == "undefined") {
+    loadScript(CDN + "lib/js/jquery.js", function() {
+      loadScript(CDN + "lib/js/mutation_summary.js", function() {
+        loadScript(CDN + "lib/js/tree_mirror.js", function() {
+          loadScript(SocketCDN + "socket.io/socket.io.js", function() {
+            window.addEventListener("load", function() {
+              startMirroring();
             });
-        })
-    }
-    else{
-        loadScript(CDN + 'lib/js/mutation_summary.js', function(){
-            loadScript(CDN + 'lib/js/tree_mirror.js', function(){
-                loadScript(SocketCDN + 'socket.io/socket.io.js', function(){
-                    window.addEventListener('load', function() {
-                        startMirroring();
-                    });
-                    init();
-                });
-            })
+            init();
+          });
         });
-    }
-}
-
-function CheckChromeFrame(){
-    loadScript('http://google.com/tools/dlpage/res/chromeframe/script/CFInstall.min.js', function(){
-        CFInstall.check({
-            mode: "popup"
-        });
+      });
     });
+  } else {
+    loadScript(CDN + "lib/js/mutation_summary.js", function() {
+      loadScript(CDN + "lib/js/tree_mirror.js", function() {
+        loadScript(SocketCDN + "socket.io/socket.io.js", function() {
+          window.addEventListener("load", function() {
+            startMirroring();
+          });
+          init();
+        });
+      });
+    });
+  }
 }
 
-function BrowserCheck(){
-    var isIE = /*@cc_on!@*/false;
-    if(isIE){
-        CheckChromeFrame();
+function CheckChromeFrame() {
+  loadScript(
+    "http://google.com/tools/dlpage/res/chromeframe/script/CFInstall.min.js",
+    function() {
+      CFInstall.check({
+        mode: "popup"
+      });
     }
-    else{
-        loadScript(CDN + 'lib/js/loader.js', function(){
-            LoadAllScripts();
-        });
-    }
+  );
+}
+
+function BrowserCheck() {
+  var isIE = /*@cc_on!@*/ false;
+  if (isIE) {
+    CheckChromeFrame();
+  } else {
+    loadScript(CDN + "lib/js/loader.js", function() {
+      LoadAllScripts();
+    });
+  }
 }
 
 BrowserCheck();
 
-function init(){
-    yepnope({
-        load : [
-            CDN + 'lib/js/session.js',
-            CDN + 'lib/css/screenshare.css'
-        ],
-        complete : function(){
-            if(sessvars.Session){
-                ContinueSession();
-            }
-            AddMenu();
-            $(window).resize(function() {
-                if(socket != undefined){
-                    socketSend({height: $(window).height(), width: $(window).width()});
-                }
-                $('#MenuTable').css({left: ($(window).width() - 30), top: ($(window).height()/2) - 150});
-            });
+function init() {
+  yepnope({
+    load: [CDN + "lib/js/session.js", CDN + "lib/css/screenshare.css"],
+    complete: function() {
+      if (sessvars.Session) {
+        ContinueSession();
+      }
+      AddMenu();
+      $(window).resize(function() {
+        if (socket != undefined) {
+          socketSend({ height: $(window).height(), width: $(window).width() });
         }
-    });
+        $("#MenuTable").css({
+          left: $(window).width() - 30,
+          top: $(window).height() / 2 - 150
+        });
+      });
+    }
+  });
 }
 
 function socketSend(msg) {
-    socket.emit('changeHappened', {change: msg, room: sessvars.Session});
+  socket.emit("changeHappened", { change: msg, room: sessvars.Session });
 }
 
 function startMirroring() {
-    var mirrorClient;
-    mirrorClient = new TreeMirrorClient(document, {
-        initialize: function(rootId, children) {
-            oDOM = {
-                f: 'initialize',
-                args: [rootId, children]
-            }
-        },
+  var mirrorClient;
+  mirrorClient = new TreeMirrorClient(document, {
+    initialize: function(rootId, children) {
+      oDOM = {
+        f: "initialize",
+        args: [rootId, children]
+      };
+    },
 
-        applyChanged: function(removed, addedOrMoved, attributes, text) {
-            if(socket != undefined){
-                socketSend({
-                    f: 'applyChanged',
-                    args: [removed, addedOrMoved, attributes, text]
-                });
-            }
-        }
-    });
-}
-function ContinueSession(){
-    socket = io.connect(SocketCDN);
-    socket.on('connect', function(){
-        socket.emit('PageChange', sessvars.Session);
-        $('#RemoteStatus').text('Status: Waiting for connection.');
-        socket.on('SessionStarted', function() {
-            $('#RemoteStatus').text('Status: Connected!');
-            socketSend({height: $(window).height(), width: $(window).width()});
-            socketSend({ base: location.href.match(/^(.*\/)[^\/]*$/)[1] });
-            socketSend(oDOM);
-            SendMouse();
-            $('body').append('<div id="AdminPointer"></div> ');
-            $(window).scroll(function(){
-                socketSend({scroll: $(window).scrollTop()});
-            });
+    applyChanged: function(removed, addedOrMoved, attributes, text) {
+      if (socket != undefined) {
+        socketSend({
+          f: "applyChanged",
+          args: [removed, addedOrMoved, attributes, text]
         });
-        socket.on('AdminMousePosition', function(msg) {
-            $('#AdminPointer').css({'left': msg.PositionLeft - 15, 'top': msg.PositionTop});
-        });
-        socket.on('DOMLoaded', function(){
-            BindEverything();
-        })
-    });
-}
-function CreateSession(){
-    socket = io.connect(SocketCDN);
-    SessionKey = document.getElementById('SessionKey').value;
-    socket.on('connect', function(){
-        socket.emit('CreateSession', SessionKey);
-        $('#RemoteStatus').text('Status: Waiting for connection.');
-        socket.on('SessionStarted', function() {
-            sessvars.Session = SessionKey;
-            $('#RemoteStatus').text('Status: Connected!');
-            socketSend({height: $(window).height(), width: $(window).width()});
-            socketSend({ base: location.href.match(/^(.*\/)[^\/]*$/)[1] });
-            socketSend(oDOM);
-            SendMouse();
-            $('body').append('<div id="AdminPointer"></div> ');
-            $(window).scroll(function(){
-                socketSend({scroll: $(window).scrollTop()});
-            });
-        });
-        socket.on('AdminMousePosition', function(msg) {
-            $('#AdminPointer').css({'left': msg.PositionLeft - 15, 'top': msg.PositionTop});
-        });
-        socket.on('DOMLoaded', function(){
-            BindEverything();
-        })
-    });
-}
-
-
-function BindEverything(){
-    $(':input').each(function(){
-        $(this).attr('value', this.value);
-    });
-    $('select').each(function(){
-        var Selected = $(this).children('option:selected');
-        $(this).children('option').removeAttr('selected', false);
-        Selected.attr('selected', true);
-        $(this).replaceWith($(this)[0].outerHTML);
-    });
-    $(':input').bind('keyup', function() {
-        $(this).attr('value', this.value);
-    });
-    $('select').change(function(){
-        var Selected = $(this).children('option:selected');
-        $(this).children('option').removeAttr('selected', false);
-        Selected.attr('selected', true);
-        $(this).replaceWith($(this)[0].outerHTML);
-        $('select').unbind('change');
-        $('select').change(function(){
-            var Selected = $(this).children('option:selected');
-            $(this).children('option').removeAttr('selected', false);
-            Selected.attr('selected', true);
-            $(this).replaceWith($(this)[0].outerHTML);
-            $('select').unbind('change');
-        });
-    });
-}
-
-
-function SendMouse(){
-    document.onmousemove = function(e) {
-        if(!e) e = window.event;
-
-        if(e.pageX == null && e.clientX != null) {
-            var doc = document.documentElement, body = document.body;
-
-            e.pageX = e.clientX
-                + (doc && doc.scrollLeft || body && body.scrollLeft || 0)
-                - (doc.clientLeft || 0);
-
-            e.pageY = e.clientY
-                + (doc && doc.scrollTop || body && body.scrollTop || 0)
-                - (doc.clientTop || 0);
-        }
-        socket.emit('ClientMousePosition', {room: SessionKey, PositionLeft: e.pageX, PositionTop: e.pageY - 5});
+      }
     }
+  });
+}
+function ContinueSession() {
+  socket = io.connect(SocketCDN);
+  socket.on("connect", function() {
+    socket.emit("PageChange", sessvars.Session);
+    $("#RemoteStatus").text("Status: Waiting for connection.");
+    socket.on("SessionStarted", function() {
+      $("#RemoteStatus").text("Status: Connected!");
+      socketSend({ height: $(window).height(), width: $(window).width() });
+      socketSend({ base: location.href.match(/^(.*\/)[^\/]*$/)[1] });
+      socketSend(oDOM);
+      SendMouse();
+      $("body").append('<div id="AdminPointer"></div> ');
+      $(window).scroll(function() {
+        socketSend({ scroll: $(window).scrollTop() });
+      });
+    });
+    socket.on("AdminMousePosition", function(msg) {
+      $("#AdminPointer").css({
+        left: msg.PositionLeft - 15,
+        top: msg.PositionTop
+      });
+    });
+    socket.on("DOMLoaded", function() {
+      BindEverything();
+    });
+  });
+}
+function CreateSession() {
+  socket = io.connect(SocketCDN);
+  SessionKey = document.getElementById("SessionKey").value;
+  socket.on("connect", function() {
+    socket.emit("CreateSession", SessionKey);
+    $("#RemoteStatus").text("Status: Waiting for connection.");
+    socket.on("SessionStarted", function() {
+      sessvars.Session = SessionKey;
+      $("#RemoteStatus").text("Status: Connected!");
+      socketSend({ height: $(window).height(), width: $(window).width() });
+      socketSend({ base: location.href.match(/^(.*\/)[^\/]*$/)[1] });
+      socketSend(oDOM);
+      SendMouse();
+      $("body").append('<div id="AdminPointer"></div> ');
+      $(window).scroll(function() {
+        socketSend({ scroll: $(window).scrollTop() });
+      });
+    });
+    socket.on("AdminMousePosition", function(msg) {
+      $("#AdminPointer").css({
+        left: msg.PositionLeft - 15,
+        top: msg.PositionTop
+      });
+    });
+    socket.on("DOMLoaded", function() {
+      BindEverything();
+    });
+  });
+}
+
+function BindEverything() {
+  $(":input").each(function() {
+    $(this).attr("value", this.value);
+  });
+  $("select").each(function() {
+    var Selected = $(this).children("option:selected");
+    $(this)
+      .children("option")
+      .removeAttr("selected", false);
+    Selected.attr("selected", true);
+    $(this).replaceWith($(this)[0].outerHTML);
+  });
+  $(":input").bind("keyup", function() {
+    $(this).attr("value", this.value);
+  });
+  $("select").change(function() {
+    var Selected = $(this).children("option:selected");
+    $(this)
+      .children("option")
+      .removeAttr("selected", false);
+    Selected.attr("selected", true);
+    $(this).replaceWith($(this)[0].outerHTML);
+    $("select").unbind("change");
+    $("select").change(function() {
+      var Selected = $(this).children("option:selected");
+      $(this)
+        .children("option")
+        .removeAttr("selected", false);
+      Selected.attr("selected", true);
+      $(this).replaceWith($(this)[0].outerHTML);
+      $("select").unbind("change");
+    });
+  });
+}
+
+function SendMouse() {
+  document.onmousemove = function(e) {
+    if (!e) e = window.event;
+
+    if (e.pageX == null && e.clientX != null) {
+      var doc = document.documentElement,
+        body = document.body;
+
+      e.pageX =
+        e.clientX +
+        ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
+        (doc.clientLeft || 0);
+
+      e.pageY =
+        e.clientY +
+        ((doc && doc.scrollTop) || (body && body.scrollTop) || 0) -
+        (doc.clientTop || 0);
+    }
+    socket.emit("ClientMousePosition", {
+      room: SessionKey,
+      PositionLeft: e.pageX,
+      PositionTop: e.pageY - 5
+    });
+  };
 }
 
 /*  ------------------------- Code for converting relative images to Data URLs -----------------------------------
@@ -264,27 +281,34 @@ function SendMouse(){
 
  */
 
-
-function AddMenu(){
-    $('body').append('<table id="MenuTable" cellpadding="0">' +
-        '<tr>' +
-        '<td><div id="SlideMenu"><p class="rotate">HELP</p></div></td>' +
-        '<td id="MainMenuTD"><h3>Remote Assistance</h3>' +
-        '<p id="RemoteAssistMessage">To Start a remote session, please create a key and provide it to the representative assisting you. Keys should be between 6 and 10 characters.</p>' +
-        '<input id="SessionKey"><p id="RemoteStatus">Status: Disconnected</p>' +
-        '<a class="btn" style="width: 70%" onclick="CreateSession();">Create Key</a>' +
-        '</td>' +
-        '</tr>' +
-        '</table>');
-    $('#MenuTable').css({left: $(window).width() - 30, top: ($(window).height()/2) - 150});
-    $('#SlideMenu').mouseenter(function(){
-        if($('#MenuTable').offset().left == $(window).width() -30){
-            $('#MenuTable').animate({left:'-=' + ($('#MenuTable').width() - 30)},'fast');
-        }
-    });
-    $('#MenuTable').mouseleave(function(){
-        if($(this).offset().left == $(window).width() - $('#MenuTable').width()){
-            $(this).animate({left:'+=' + ($('#MenuTable').width() - 30)},'fast');
-        }
-    });
+function AddMenu() {
+  $("body").append(
+    '<table id="MenuTable" cellpadding="0">' +
+      "<tr>" +
+      '<td><div id="SlideMenu"><p class="rotate">HELP</p></div></td>' +
+      '<td id="MainMenuTD"><h3>Remote Assistance</h3>' +
+      '<p id="RemoteAssistMessage">To Start a remote session, please create a key and provide it to the representative assisting you. Keys should be between 6 and 10 characters.</p>' +
+      '<input id="SessionKey"><p id="RemoteStatus">Status: Disconnected</p>' +
+      '<a class="btn" style="width: 70%" onclick="CreateSession();">Create Key</a>' +
+      "</td>" +
+      "</tr>" +
+      "</table>"
+  );
+  $("#MenuTable").css({
+    left: $(window).width() - 30,
+    top: $(window).height() / 2 - 150
+  });
+  $("#SlideMenu").mouseenter(function() {
+    if ($("#MenuTable").offset().left == $(window).width() - 30) {
+      $("#MenuTable").animate(
+        { left: "-=" + ($("#MenuTable").width() - 30) },
+        "fast"
+      );
+    }
+  });
+  $("#MenuTable").mouseleave(function() {
+    if ($(this).offset().left == $(window).width() - $("#MenuTable").width()) {
+      $(this).animate({ left: "+=" + ($("#MenuTable").width() - 30) }, "fast");
+    }
+  });
 }
